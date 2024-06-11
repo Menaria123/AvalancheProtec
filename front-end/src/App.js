bc54d567649a97e5536b652039d4b59b7bcee0d2
Kusum Menaria <104812753+Menaria123@users.noreply.github.com>
2024-06-11 11:49:03 +0530

    Add files via upload
    Signed-off-by: Kusum Menaria <104812753+Menaria123@users.noreply.github.com>

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,16 @@
+// src/App.js
 import React from 'react';
-import Navbar from './navbar';
-import Landingpage from './Landing_page'
-import NDRF from './ndrf';
-import Safe from './safty_guide';
-import Area from './Area-wise-analyisi';
-import Map from './map'
-import InfoBox from './InfoBox';
-// Main App component
+import './App.css';
+import  MapComponent from './map';
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Landingpage />
-      <Map />
-      <InfoBox />
-      <Area />
-      <NDRF />
-      <Safe />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <h1>Leaflet Map in React</h1>
+      </header>
+      <MapComponent />
+    </div>
   );
 }
 
